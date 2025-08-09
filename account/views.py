aa0937fc17462d05ea97fdd821d31c56b1164533
Hari prasad <mhariprasad2001@gmail.com>
2025-08-09 23:09:50 +0530

    25332

--- account/views.py
+++ account/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
-
-# Create your views here.
+<!DOCUMENT html>
+<html>
+<tite>My Homepage</title>
+<style>
+body{
+    font-family: 'Segoe UI' Tahoma, Sans-Serif;
+    background-color: #f4f6f8;
+    color: #333;
+    margin: 0;
+}
+header {
+    background: #007BFF;
+    color: white;
+    padding: 1rem;
+    text-agin: center;
+    font-size: 1.5rem;
+}
+main {
+    padding: 2rem;
+    text-agin: center;
+}
+h1{
+    font-size: 2.5rem;
+    color: #007BFF;
+}
+p{
+    font-size: 1.2rem;
+    color: #555;
+}
+</style>
+</head>
+<body>
+<header> My note</header>
+<main>
+<h1> Welccome to My Note</h1>
+<p> Organize Your thought, ideas </p>
+<a herf="/Login" class ="bth">Get Started</a>
+</main>
+</body>
+</html>
