1d94bf5d6894b02b77e8b45c7b54022614c2b04e
Hari prasad <mhariprasad2001@gmail.com>
2025-08-12 12:32:29 +0530

    25333

--- account/apps.py
+++ account/apps.py
@@ -1,6 +1,27 @@
-from django.apps import AppConfig
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Not Found</title>
+        <style>
+                body {
+                            font-family: Arial, sans-serif;
+                                        text-align: center;
+                                                    padding: 50px;
+                                                            }
+                                                                    h1 {
+                                                                                color: #555;
+                                                                                        }
+                                                                                                p {
+                                                                                                            color: #777;
+                                                                                                                    }
+                                                                                                                        </style>
+                                                                                                                        </head>
+                                                                                                                        <body>
+                                                                                                                            <h1>404 - Page Not Found</h1>
+                                                                                                                                <p>The page you're looking for doesn't exist.</p>
+                                                                                                                                    <a href="{% url 'home' %}">Go back to homepage</a>
+                                                                                                                                    </body>
+                                                                                                                                    </html>
 
-
-class AccountConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'account'
+                                                                                                                                    DEBUG = False
+                                                                                                                                    ALLOWED_HOSTS = ['yourdomain.com', 'localhost']
