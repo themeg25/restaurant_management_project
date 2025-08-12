e295b8f153a53f773adfabfd93c5a0d0cb8a764a
Hari prasad <mhariprasad2001@gmail.com>
2025-08-12 12:38:30 +0530

    25335

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
+# models.py
+from django.db import models
 
-# Register your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    # views.py
+    from django.shortcuts import render
+    from .models import Restaurant
+
+    def homepage(request):
+        restaurant = Restaurant.objects.first()  # Fetch the first restaurant
+            return render(request, 'homepage.html', {'restaurant_name': restaurant.name})
