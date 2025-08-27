a87738baecc27038ec245b0c1aa784024bb66fcc
Hari prasad <mhariprasad2001@gmail.com>
2025-08-27 19:03:35 +0530

    38035

--- home/__init__.py
+++ home/__init__.py
@@ -1,14 +1,9 @@
-     <!# views.py
-     from django.shortcuts import render
+# views.py
+from django.conf import settings
+from django.shortcuts import render
 
-     def contact_us(request):
-         return render(request, 'contact_us.html')
-         <!-- contact_us.html -->
-         <h1>Contact Us</h1>
-         <p>We'd love to hear from you!</p>
-         <ul>
-             <li><strong>Address:</strong> 123 Restaurant St, Food City</li>
-                 <li><strong>Phone:</strong> (123) 456-7890</li>
-                     <li><strong>Email:</strong> info@ourrestaurant.com</li>
-                     </ul>
-                                                                                                                              
+def homepage(request):
+    phone_number = settings.RESTAURANT_PHONE_NUMBER
+        return render(request, 'homepage.html', {'phone_number': phone_number})
+        
+                                                                                         
