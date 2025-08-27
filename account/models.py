a06bfac8ccdba7fe938293a5e49488a1b215d1b5
Hari prasad <mhariprasad2001@gmail.com>
2025-08-27 18:56:06 +0530

    38032

--- account/models.py
+++ account/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+# views.py
+from django.shortcuts import render
 
-# Create your models here.
+def menu_view(request):
+    # Hardcoded menu items for simplicity
+        menu_items = [
+                {'name': 'Burger', 'description': 'Juicy beef burger', 'price': 8.99},
+                        {'name': 'Pizza', 'description': 'Cheese pizza with veggies', 'price': 10.99},
+                                {'name': 'Salad', 'description': 'Fresh garden salad', 'price': 6.99},
+                                    ]
+                                        return render(request, 'menu.html', {'menu_items': menu_items})                                                                            quantity = models.PositiveIntegerField(default=1)
