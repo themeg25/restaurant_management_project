f8915518e413a892cfb655583debd8dde8070011
Hari prasad <mhariprasad2001@gmail.com>
2025-08-29 19:38:33 +0530

    38036

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
+# views.py
+from django.conf import settings
+from django.shortcuts import render
 
-# Create your tests here.
+def homepage(request):
+    restaurant_name = settings.RESTAURANT_NAME
+        return render(request, 'homepage.html', {'restaurant_name': restaurant_name})
