45fba611eade20c952d6cc37bd23f7157a35d084
Hari prasad <mhariprasad2001@gmail.com>
2025-09-17 19:35:31 +0530

    42902

--- account/apps.py
+++ account/apps.py
@@ -1,6 +1,9 @@
-from django.apps import AppConfig
-
-
-class AccountConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'account'
+<!-- contact.html -->
+<form method="post">
+    {% csrf_token %}
+        <input type="text" name="name" required placeholder="Your name">
+            <input type="email" name="email" required placeholder="Your email">
+                <textarea name="message" required placeholder="Your message"></textarea>
+                    <button type="submit">Submit</button>
+                    </form>
+                                                                                 ALLOWED_HOSTS = ['yourdomain.com', 'localhost']
